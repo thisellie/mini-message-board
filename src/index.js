29b2330a0864f58b69c2a6ca29d10dc59c206b5d
thisellie <81550623+thisellie@users.noreply.github.com>
2025-05-28 19:10:57 +0800

    Update package.json and index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const routes = require('./routes/home')
+
+const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -8,6 +11,6 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use('/', routes)
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
 })
